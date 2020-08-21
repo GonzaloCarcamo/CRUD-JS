a2ce9a1150a6e7a4408a2cfa379ffc2a333e0554
goncmodev <gon.cmo.dev@gmail.com>
2020-08-21 23:07:26 +0200

    Crud terminado

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,9 +12,38 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/add', async (req, res) => {
-const task = new Task(req.body);
-await task.save();
-res.send('recibido');
+    const task = new Task(req.body);
+    await task.save();
+    res.redirect('/');
+});
+
+router.get('/turn/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/')
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.render('edit', {
+        task
+    });
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.update( { _id: id}, req.body);
+    res.redirect('/');
+
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await Task.remove({ _id: id });
+    res.redirect('/');
 });
 
 module.exports = router;
